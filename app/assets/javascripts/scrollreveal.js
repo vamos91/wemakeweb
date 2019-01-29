221a1f19eb9658ae1231083c17dca50210b509ab
nicolas <acardnicolas@hotmail.com>
2019-01-29 17:22:52 +0100

    mon site web

--- app/assets/javascripts/scrollreveal.js
+++ app/assets/javascripts/scrollreveal.js
@@ -1,8 +1,8 @@
-window.sr = ScrollReveal()
-sr.reveal('.nom', { origin:"bottom", distance: '50px', duration: 1000, reset:false });
-sr.reveal('.base-line', { origin:"bottom", distance: '50px', duration: 1000, delay: 500, reset:false });
-sr.reveal('.slogan', { origin:"bottom", distance: '50px', duration: 1000, delay: 1000, reset:false });
-sr.reveal('.bouton_header', { origin:"bottom", distance: '50px', duration: 1000, delay: 1500, reset:false });
-sr.reveal('.bandeau-banner', { origin:"left", distance: '50px', duration: 1000, delay: 2000, reset:false });
-sr.reveal('.feature', { origin:"bottom", distance: '50px', duration: 500, delay: 500, reset:false });
+// window.sr = ScrollReveal()
+// sr.reveal('.nom', { origin:"bottom", distance: '50px', duration: 1000, reset:false });
+// sr.reveal('.base-line', { origin:"bottom", distance: '50px', duration: 1000, delay: 500, reset:false });
+// sr.reveal('.slogan', { origin:"bottom", distance: '50px', duration: 1000, delay: 1000, reset:false });
+// sr.reveal('.bouton_header', { origin:"bottom", distance: '50px', duration: 1000, delay: 1500, reset:false });
+// sr.reveal('.bandeau-banner', { origin:"left", distance: '50px', duration: 1000, delay: 2000, reset:false });
+// sr.reveal('.feature', { origin:"bottom", distance: '50px', duration: 500, delay: 500, reset:false });
 
