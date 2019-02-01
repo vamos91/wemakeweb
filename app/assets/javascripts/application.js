46d3b0555fb837a881e9059e635407367b002f7a
nicolas <acardnicolas@hotmail.com>
2019-02-01 12:51:40 +0100

    bouton update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,21 @@
+//= require jquery
+
+
 //= require rails-ujs
 //= require_tree .
+
+
+
+var menu = 0;
+function close_banner(){
+  if (menu == 0)
+  {
+    $('.banner-bg-video').animate({width: '81%'}, 800);
+    // $('.banner-bg-video').addClass('banner-bg-video-retract');
+    menu = 1;
+  }
+  else{
+    $('.banner-bg-video').animate({width: '100%'}, 800);
+    menu = 0;
+  }
+};
--- app/assets/javascripts/scrollreveal.js
+++ app/assets/javascripts/scrollreveal.js
@@ -1,8 +1,11 @@
-// window.sr = ScrollReveal()
-// sr.reveal('.nom', { origin:"bottom", distance: '50px', duration: 1000, reset:false });
-// sr.reveal('.base-line', { origin:"bottom", distance: '50px', duration: 1000, delay: 500, reset:false });
-// sr.reveal('.slogan', { origin:"bottom", distance: '50px', duration: 1000, delay: 1000, reset:false });
-// sr.reveal('.bouton_header', { origin:"bottom", distance: '50px', duration: 1000, delay: 1500, reset:false });
-// sr.reveal('.bandeau-banner', { origin:"left", distance: '50px', duration: 1000, delay: 2000, reset:false });
-// sr.reveal('.feature', { origin:"bottom", distance: '50px', duration: 500, delay: 500, reset:false });
+window.sr = ScrollReveal()
+sr.reveal('.nom', { origin:"bottom", distance: '50px', duration: 1000, reset:false });
+sr.reveal('.base-line', { origin:"bottom", distance: '50px', duration: 1000, delay: 500, reset:false });
+sr.reveal('.slogan', { origin:"bottom", distance: '50px', duration: 1000, delay: 1000, reset:false });
+sr.reveal('.bouton1', { origin:"left", distance: '50px', duration: 1000, delay: 2000, reset:false });
+sr.reveal('.bouton2', { origin:"right", distance: '50px', duration: 1000, delay: 1500, reset:false });
+sr.reveal('.bandeau-banner', { origin:"left", distance: '50px', duration: 1000, delay: 2000, reset:false });
+sr.reveal('.feature1', { origin:"bottom", distance: '50px', duration: 500, delay: 500, reset:false });
+sr.reveal('.feature2', { origin:"bottom", distance: '50px', duration: 500, delay: 800, reset:false });
+sr.reveal('.feature3', { origin:"bottom", distance: '50px', duration: 500, delay: 1100, reset:false });
 
