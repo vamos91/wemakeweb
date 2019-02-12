9ed7093c91ce6c87afe904e47ec96209ab06aac1
nicolas <acardnicolas@hotmail.com>
2019-02-12 15:16:29 +0100

    mobile setting

--- app/assets/javascripts/scrollreveal.js
+++ app/assets/javascripts/scrollreveal.js
@@ -15,4 +15,4 @@ sr.reveal('.feature_service3', { origin:"bottom", distance: '50px', duration: 50
 sr.reveal('.feature_service4', { origin:"bottom", distance: '50px', duration: 500, delay: 1400, reset:false });
 sr.reveal('.feature_service5', { origin:"bottom", distance: '50px', duration: 500, delay: 1700, reset:false });
 sr.reveal('.feature_service6', { origin:"bottom", distance: '50px', duration: 500, delay: 2000, reset:false });
-sr.reveal('.container-contact100-form-btn', { origin:"left", distance: '50px', duration: 500, delay: 2000, reset:false });
+sr.reveal('.container-contact100-form-btn', { origin:"bottom", distance: '50px', duration: 500, delay: 500, reset:false });
